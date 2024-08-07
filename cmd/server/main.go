f2c8333fd6d20bb3f003e30ec91fc02c87d57448
zhiyuanGH <zhi-yuan.hu@connect.polyu.hk>
2024-08-07 18:57:54 +0800

    get source of nfs, ready for test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -23,151 +23,161 @@ import (
 )
 
 type server struct {
-    pb.UnimplementedContainerMigrationServer
+	pb.UnimplementedContainerMigrationServer
 }
 
 func (s *server) CheckpointContainer(ctx context.Context, req *pb.CheckpointRequest) (*pb.CheckpointResponse, error) {
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        return nil, err
-    }
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
 
-    // Inspect the container to get the full ID
-    containerInfo, err := cli.ContainerInspect(ctx, req.ContainerId)
-    if err != nil {
-        return nil, err
-    }
-    fullContainerID := containerInfo.ID
+	// Inspect the container to get the full ID
+	containerInfo, err := cli.ContainerInspect(ctx, req.ContainerId)
+	if err != nil {
+		return nil, err
+	}
+	fullContainerID := containerInfo.ID
 
-    checkpointID := fmt.Sprintf("checkpoint_%d", time.Now().Unix())
-    if err := cli.CheckpointCreate(ctx, req.ContainerId, checkpoint.CreateOptions{CheckpointID: checkpointID, Exit: true}); err != nil {
-        return nil, err
-    }
+	checkpointID := fmt.Sprintf("checkpoint_%d", time.Now().Unix())
+	if err := cli.CheckpointCreate(ctx, req.ContainerId, checkpoint.CreateOptions{CheckpointID: checkpointID, Exit: true}); err != nil {
+		return nil, err
+	}
 
-    checkpointDir := fmt.Sprintf("/var/lib/docker/containers/%s/checkpoints/%s", fullContainerID, checkpointID)
-    var buf bytes.Buffer
-    gz := gzip.NewWriter(&buf)
-    tarWriter := tar.NewWriter(gz)
+	checkpointDir := fmt.Sprintf("/var/lib/docker/containers/%s/checkpoints/%s", fullContainerID, checkpointID)
+	var buf bytes.Buffer
+	gz := gzip.NewWriter(&buf)
+	tarWriter := tar.NewWriter(gz)
 
-    err = filepath.Walk(checkpointDir, func(file string, fi os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        hdr, err := tar.FileInfoHeader(fi, file)
-        if err != nil {
-            return err
-        }
-        hdr.Name = filepath.ToSlash(file[len(checkpointDir):])
-        if err := tarWriter.WriteHeader(hdr); err != nil {
-            return err
-        }
-        if !fi.Mode().IsRegular() {
-            return nil
-        }
-        f, err := os.Open(file)
-        if err != nil {
-            return err
-        }
-        defer f.Close()
-        if _, err := io.Copy(tarWriter, f); err != nil {
-            return err
-        }
-        return nil
-    })
-    if err != nil {
-        return nil, err
-    }
+	err = filepath.Walk(checkpointDir, func(file string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		hdr, err := tar.FileInfoHeader(fi, file)
+		if err != nil {
+			return err
+		}
+		hdr.Name = filepath.ToSlash(file[len(checkpointDir):])
+		if err := tarWriter.WriteHeader(hdr); err != nil {
+			return err
+		}
+		if !fi.Mode().IsRegular() {
+			return nil
+		}
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		if _, err := io.Copy(tarWriter, f); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
 
-    if err := tarWriter.Close(); err != nil {
-        return nil, err
-    }
-    if err := gz.Close(); err != nil {
-        return nil, err
-    }
+	if err := tarWriter.Close(); err != nil {
+		return nil, err
+	}
+	if err := gz.Close(); err != nil {
+		return nil, err
+	}
 
-    return &pb.CheckpointResponse{CheckpointId: checkpointID, CheckpointData: buf.Bytes()}, nil
+	return &pb.CheckpointResponse{CheckpointId: checkpointID, CheckpointData: buf.Bytes()}, nil
 }
 
 func (s *server) TransferVolume(ctx context.Context, req *pb.VolumeRequest) (*pb.VolumeResponse, error) {
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        return nil, err
-    }
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
 
-    containerID := req.ContainerId
-    containerInfo, err := cli.ContainerInspect(ctx, containerID)
-    if err != nil {
-        return nil, err
-    }
+	containerID := req.ContainerId
+	containerInfo, err := cli.ContainerInspect(ctx, containerID)
+	if err != nil {
+		return nil, err
+	}
 
-    var volumeName string
-    var nfsSource string
+	var volumeName string //volumeName is only assigned if the container has a local volume
+	var nfsSource string  //nfsSource is only assigned if the container has a nfs bind mount
 
-    for _, mount := range containerInfo.Mounts {
-        if mount.Type == "volume" {
-            volumeName = mount.Name
-            break
-        }
-        if mount.Type == "bind" {
-            source, err := getMountSource(mount.Source)
-            if err != nil {
-                return nil, err
-            }
-            nfsSource = source
-            break
-        }
-    }
+	if len(containerInfo.Mounts) == 0 {
+		return nil, fmt.Errorf("no mounts found for container: %s", containerID)
+	}
+	var destination string
 
-    if volumeName != "" {
-    volume, err := cli.VolumeInspect(ctx, volumeName)
-    if err != nil {
-        return nil, err
-    }
+	for _, mount := range containerInfo.Mounts {
 
-    volumeDir := volume.Mountpoint
-    var buf bytes.Buffer
-    gz := gzip.NewWriter(&buf)
-    tarWriter := tar.NewWriter(gz)
+		if mount.Type == "volume" {
+			volumeName = mount.Name
+			break
+		}
+		if mount.Type == "bind" {
+			source, err := getMountSource(mount.Source)
+			if err != nil {
+				return nil, err
+			}
+			nfsSource = source
+			break
+		}
+		destination = mount.Destination // assign the value to destination
+	}
 
-    err = filepath.Walk(volumeDir, func(file string, fi os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        hdr, err := tar.FileInfoHeader(fi, file)
-        if err != nil {
-            return err
-        }
-        hdr.Name = filepath.ToSlash(file[len(volumeDir):])
-        if err := tarWriter.WriteHeader(hdr); err != nil {
-            return err
-        }
-        if !fi.Mode().IsRegular() {
-            return nil
-        }
-        f, err := os.Open(file)
-        if err != nil {
-            return err
-        }
-        defer f.Close()
-        if _, err := io.Copy(tarWriter, f); err != nil {
-            return err
-        }
-        return nil
-    })
-    if err != nil {
-        return nil, err
-    }
+	// If the container has a local volume, transfer the volume data
+	if volumeName != "" {
+		volume, err := cli.VolumeInspect(ctx, volumeName)
+		if err != nil {
+			return nil, err
+		}
 
-    if err := tarWriter.Close(); err != nil {
-        return nil, err
-    }
-    if err := gz.Close(); err != nil {
-        return nil, err
-    }
+		volumeDir := volume.Mountpoint
+		var buf bytes.Buffer
+		gz := gzip.NewWriter(&buf)
+		tarWriter := tar.NewWriter(gz)
 
-    return &pb.VolumeResponse{VolumeName: volumeName, VolumeData: buf.Bytes()}, nil
-}
-return &pb.VolumeResponse{VolumeName: volumeName, NfsSource: nfsSource}, nil
+		err = filepath.Walk(volumeDir, func(file string, fi os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			hdr, err := tar.FileInfoHeader(fi, file)
+			if err != nil {
+				return err
+			}
+			hdr.Name = filepath.ToSlash(file[len(volumeDir):])
+			if err := tarWriter.WriteHeader(hdr); err != nil {
+				return err
+			}
+			if !fi.Mode().IsRegular() {
+				return nil
+			}
+			f, err := os.Open(file)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			if _, err := io.Copy(tarWriter, f); err != nil {
+				return err
+			}
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		if err := tarWriter.Close(); err != nil {
+			return nil, err
+		}
+		if err := gz.Close(); err != nil {
+			return nil, err
+		}
+
+		return &pb.VolumeResponse{VolumeName: volumeName, VolumeData: buf.Bytes(),Destination: destination}, nil
+	}
+	// If the container has a nfs bind mount, return the NFS source
+	return &pb.VolumeResponse{NfsSource: nfsSource, Destination: destination}, nil
+
 }
 
 func getMountSource(mountPoint string) (string, error) {
@@ -194,17 +204,17 @@ func getMountSource(mountPoint string) (string, error) {
 }
 
 func main() {
-    lis, err := net.Listen("tcp4", "0.0.0.0:50051") // Use "tcp4" to force IPv4
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
+	lis, err := net.Listen("tcp4", "0.0.0.0:50051") // Use "tcp4" to force IPv4
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
-    grpcServer := grpc.NewServer()
-    pb.RegisterContainerMigrationServer(grpcServer, &server{})
-    log.Printf("Server listening at %v", lis.Addr())
-    if err := grpcServer.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
-    }
+	grpcServer := grpc.NewServer()
+	pb.RegisterContainerMigrationServer(grpcServer, &server{})
+	log.Printf("Server listening at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
 
 //hi
