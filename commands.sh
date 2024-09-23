dc43845a422c7c1bab88e47ad3367a2c7877ccc4
zhiyuanGH <zhi-yuan.hu@connect.polyu.hk>
2024-09-23 17:09:35 +0800

    revise the run cmd

--- commands.sh
+++ commands.sh
@@ -5,4 +5,4 @@ sudo mount 192.168.116.148:/srv/nfs/share /mnt/nfs_share
 sudo umount /mnt/nfs_share
 source /etc/profile
 protoc --go_out=. --go-grpc_out=. proto/container.proto
-docker run --name cnn -v /srv/nfs/share:/data 192.168.1.102:5000/mnist-rnn-image:org python3 -u main.py --batch-size 64 --test-batch-size 1000 --epochs 3 --lr 0.1 --gamma 0.7 --log-interval 1 --save-model
+docker run --name cnn -v /mnt/nfs_share:/data 192.168.1.102:5000/mnist-rnn-image:org python3 -u main.py --batch-size 64 --test-batch-size 1000 --epochs 3 --lr 0.1 --gamma 0.7 --log-interval 1 --save-model
